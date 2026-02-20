67d78373bb8dbaf000c5c23e44bd41dbd185c433
Arian Spivey <ARIANS@email.uscb.edu>
2026-02-19 19:36:07 -0500

    Final commit

--- src/Deitel6_6ProblemApp.java
+++ src/Deitel6_6ProblemApp.java
@@ -6,6 +6,22 @@ public class Deitel6_6ProblemApp {
 
     public static void main(String[] args) {
 
-       
+        // Initialization Phase
+        int[] values = {5, 10, 15, 20, 25};
+        int[] copy = new int[values.length];
+
+        // Processing Phase: copy array
+        for (int i = 0; i < values.length; i++) {
+            copy[i] = values[i];
+        }
+
+        // Display results 
+        System.out.println("Index\tValues\tCopy");
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(i + "\t" + values[i] + "\t" + copy[i]);
+        }
+
+        /*  Termination Phase */
+        System.out.println("Done.");
     }
 }
